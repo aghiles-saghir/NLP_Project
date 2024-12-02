96a1e709009cb53b8b11d832190e2bde767baf3f
amayas mahmoudi <amayasmah562@gmail.com>
2024-12-02 10:31:26 +0100

    Load data + filtring columns

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
-import spacy
-from spacy.lang.en.examples import sentences
+import pandas as pd
+import json
 
-nlp = spacy.load("en_core_web_sm")
-doc = nlp(sentences[0])
-print(doc.text)
-for token in doc:
-    print(token.text, token.pos_, token.dep_)
+# Fonction pour charger un fichier JSONL
+def load_jsonl(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return [json.loads(line) for line in f]
+
+# Charger les fichiers
+reviews_file_path = './data/reviews.jsonl'
+meta_file_path = './data/meta.jsonl'
+
+reviews_data = load_jsonl(reviews_file_path)
+meta_data = load_jsonl(meta_file_path)
+
+# Convertir en DataFrame
+reviews_df = pd.DataFrame(reviews_data)
+meta_df = pd.DataFrame(meta_data)
+
+# Sélectionner les champs pertinents
+reviews_selected = reviews_df[['title', 'text']]  # Champs pertinents des avis
+meta_selected = meta_df[['main_category', 'title', 'average_rating', 'rating_number']]  # Champs pertinents des métadonnées
+
+# Afficher un aperçu des données
+print("Reviews DataFrame:")
+print(reviews_selected.head())
+
+print("\nMeta DataFrame:")
+print(meta_selected.head())
+
